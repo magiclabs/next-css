b73422b3181cb14b750053421e8fa716c016074a
Ian K Smith <smithki707@gmail.com>
2021-02-25 18:31:42 -0500

    Improve error message formatting

--- src/utils/errors-warnings.ts
+++ src/utils/errors-warnings.ts
@@ -4,12 +4,13 @@ import chalk from 'chalk';
 import wrapAnsi from 'wrap-ansi';
 
 export function createMessage(type: 'warning' | 'error', message: string, helpLink?: string) {
-  const label = type === 'error' ? chalk`{red.bold ------- error -------}` : chalk`{yellow.bold ------ warning ------}`;
-  const pkg = chalk`\n{dim [@magiclabs/next-css]}`;
-  const suffix = helpLink ? `\nRead more: ${helpLink}` : '';
+  const label =
+    type === 'error' ? chalk`{red.bold ┏━━━━━━━ error ━━━━━━━}` : chalk`{yellow.bold ┏━━━━━━ warning ━━━━━━}`;
+  const pkg = chalk`\n {dim [@magiclabs/next-css]}`;
+  const suffix = helpLink ? `\n\n Read more: ${helpLink}` : '';
 
-  const maxWidth = process.stdout.columns <= 80 ? process.stdout.columns : 80;
-  return `${label}${pkg}\n${wrapAnsi(message, maxWidth)}${suffix}\n`;
+  const maxWidth = process.stdout.columns <= 79 ? process.stdout.columns : 79;
+  return `${label}${pkg}\n ${wrapAnsi(message, maxWidth).split('\n').join('\n ')}${suffix}\n`;
 }
 
 export function printMessage(type: 'warning' | 'error', message: string, helpLink?: string) {
